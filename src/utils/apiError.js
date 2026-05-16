6bae5e78d507df999708a09ba3b37309230fddc9
Pawan Kharel <163934527+kharpaw@users.noreply.github.com>
2026-05-16 18:29:42 +0300

    utis configure

---
+++ src/utils/apiError.js
@@ -0,0 +1,25 @@
+class Apierror extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck= ""
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.sucess = false;
+        this.error = errors
+
+
+        if (statck) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+            
+    }
+}
+
+export {Apierror}
